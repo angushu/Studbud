ea4238e0cad45616705bab60e9867bda2ed9d2f9
Angus Hu <anhu4277@uni.sydney.edu.au>
2022-05-06 13:28:31 +1000

    added node and express

---
+++ server.js
@@ -0,0 +1,18 @@
+// importing the express package
+const express = require('express');
+
+// initialising the express framework
+const app = express();
+
+// serving static files from the public folder
+app.use(express.static('public'));
+
+// serving the index from the root path
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+   })
+
+// starting the server,listening for incoming traffic and logging a message to the console
+let server = app.listen(8888, function(){
+    console.log("App server is running on port 8888");
+   });
