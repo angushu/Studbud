1190b11639087ad802824b18e8a97d7a36cc3dce
Angus Hu <anhu4277@uni.sydney.edu.au>
2022-05-06 13:37:24 +1000

    installed parcel

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const express = require('express');
 const app = express();
 
 // serving static files from the public folder
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/dist'));
 
 // serving the index from the root path
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
    })
 
 // starting the server,listening for incoming traffic and logging a message to the console
