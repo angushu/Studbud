f5d046fcf20f691ddb01c4f2a95f74d99f0b17af
Angus Hu <anhu4277@uni.sydney.edu.au>
2022-05-12 13:09:01 +1000

    implemented working stopwatch

--- dist/index.3b06b0f7.js
+++ dist/index.3b06b0f7.js
@@ -616,6 +616,44 @@ function updateEmpty() {
 }
 
 },{}],"hKQ2O":[function(require,module,exports) {
+/* Stopwatch created with inspiration from:
+https://www.youtube.com/watch?v=49f1cjZWRJA&ab_channel=TylerPotts */ // Global Variables
+const elapsedTime = document.getElementById('watch-face');
+const start_btn = document.getElementById('start');
+const stop_btn = document.getElementById('stop');
+const reset_btn = document.getElementById('reset');
+let seconds = 0;
+let interval = null;
+// Event Listeners
+start_btn.addEventListener('click', start);
+stop_btn.addEventListener('click', stop);
+reset_btn.addEventListener('click', reset);
+// Timer Function for ticking up seconds and the display of time
+// Update the timer
+function timer() {
+    seconds++;
+    // Formatting our Time
+    let hrs = Math.floor(seconds / 3600);
+    let mins = Math.floor((seconds - hrs * 3600) / 60);
+    let secs = seconds % 60;
+    if (secs < 10) secs = "0" + secs;
+    if (mins < 10) mins = "0" + mins;
+    if (hrs < 10) hrs = "0" + hrs;
+    elapsedTime.innerText = `${hrs}:${mins}:${secs}`;
+}
+function start() {
+    if (interval) return;
+    interval = setInterval(timer, 1000);
+}
+function stop() {
+    clearInterval(interval);
+    interval = null;
+}
+function reset() {
+    stop();
+    seconds = 0;
+    elapsedTime.innerText = "00:00:00";
+}
 
 },{}],"kfsoy":[function(require,module,exports) {
 
