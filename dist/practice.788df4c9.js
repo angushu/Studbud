b6cd6ee2b3e646433a323adbd1c18f446a4218bd
Angus Hu <anhu4277@uni.sydney.edu.au>
2022-05-13 11:54:29 +1000

    added gitignorefile

--- dist/practice.788df4c9.js
+++ dist/practice.788df4c9.js
@@ -570,11 +570,6 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
     output.innerHTML = this.value;
 };
-slider.addEventListener("mousemove", function() {
-    var x = slider.value;
-    var color = 'linear-gradient(90deg, rgb(117,252,117)' + x + '%, rgb(214,214,214)' + x + '%)';
-    slider.style.background = color;
-});
 
 },{}]},["fBTxV","hKQ2O"], "hKQ2O", "parcelRequire60da")
 
--- public/components/practice.js
+++ public/components/practice.js
@@ -62,9 +62,3 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
     output.innerHTML = this.value;
 }
-
-slider.addEventListener("mousemove", function(){
-var x = slider.value;
-var color = 'linear-gradient(90deg, rgb(117,252,117)' + x + '%, rgb(214,214,214)' + x + '%)'
-slider.style.background = color;
-})
